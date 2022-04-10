18656315bfdf6ed1aa7be63e9333857e78d1cf1d
Limacus <daviesliam021@gmail.com>
2022-04-10 20:25:20 +0100

    RS

--- header.php
+++ header.php
@@ -35,20 +35,20 @@
         <meta name="theme-color" content="#FFFFFF">
 
         <!-- Page canonical URL -->
-        <link rel="canonical" href="http://167.71.140.97/">
+        <link rel="canonical" href="https://impf1.co.uk/">
 
         <!-- Icons -->
-        <link rel="icon" href="http://167.71.140.97/media-uploads/IMP_lines.png/" type="image/x-icon"> 
-        <link rel="shortcut icon" href="http://167.71.140.97/media-uploads/IMP_lines.png/" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="192x192" href="http://167.71.140.97/media-uploads/IMP_lines.png/">
-        <link rel="icon" type="image/png" sizes="32x32" href="http://167.71.140.97/media-uploads/IMP_lines.png/">
-        <link rel="icon" type="image/png" sizes="16x16" href="http://167.71.140.97/media-uploads/IMP_lines.png/">
+        <link rel="icon" href="https://impf1.co.uk/media/IMP_lines.png/" type="image/x-icon"> 
+        <link rel="shortcut icon" href="https://impf1.co.uk/media/IMP_lines.png/" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="192x192" href="https://impf1.co.uk/media/IMP_lines.png/">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://impf1.co.uk/IMP_lines.png/">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://impf1.co.uk/media/IMP_lines.png/">
 
         <!-- Facebook Open Graph -->
         <meta property="og:title" content="IMPF1 - Professional Ro-Racing">
         <meta property="og:type" content="IMPF1">
-        <meta property="og:url" content="http://167.71.140.97/">
-        <meta property="og:image" content="http://167.71.140.97/media-uploads/IMPLOGONEW.png">
+        <meta property="og:url" content="https://impf1.co.uk/">
+        <meta property="og:image" content="https://impf1.co.uk/media/IMPLOGONEW.png">
         <meta property="og:image:alt" content="IMPF1 logo">
         <meta property="og:description" content="Welcome to IMPF1 - The home of professional Ro-Racing. Looking for a friendly and fun league with responsive cars and exceptional track design? You've come to the right place.">
         <meta property="og:site_name" content="IMPF1">
@@ -58,10 +58,10 @@
         <meta name="twitter:card" content="summary">
         <meta name="twitter:site" content="@website-account">
         <meta name="twitter:creator" content="@personal-account">
-        <meta name="twitter:url" content="http://167.71.140.97/">
+        <meta name="twitter:url" content="https://impf1.co.uk/">
         <meta name="twitter:title" content="IMPF1 - Professional Ro-Racing">
         <meta name="twitter:description" content="Welcome to IMPF1 - The home of professional Ro-Racing. Looking for a friendly and fun league with responsive cars and exceptional track design? You've come to the right place.">
-        <meta name="twitter:image" content="http://167.71.140.97/media-uploads/IMPLOGONEW.png">
+        <meta name="twitter:image" content="https://impf1.co.uk/media/IMPLOGONEW.png">
         <meta name="twitter:image:alt" content="IMPF1 logo">
 </head>
 
