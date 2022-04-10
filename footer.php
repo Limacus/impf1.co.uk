6688914e45daf2639c735d128cd40767cacc9816
Limacus <daviesliam021@gmail.com>
2022-04-10 19:58:28 +0100

    RS

--- footer.php
+++ footer.php
@@ -13,17 +13,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'impf1' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'impf1' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'impf1' ), 'impf1', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
