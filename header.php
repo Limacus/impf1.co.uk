d9b2458364bdcdecaa27c280ddf3aac4b78c6f3a
Limacus <daviesliam021@gmail.com>
2022-04-10 20:11:50 +0100

    RS

--- header.php
+++ header.php
@@ -72,22 +72,7 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$impf1_description = get_bloginfo( 'description', 'display' );
-			if ( $impf1_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $impf1_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			<img src="media/IMPLOGONEW.png" alt="IMP logo" />
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
